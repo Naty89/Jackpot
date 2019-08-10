914d3ae4f54569770cea351995f6690101f28fac
Naty89 <37753984+Naty89@users.noreply.github.com>
2019-08-10 13:51:15 -0400

    Update testJackpot.py

--- testJackpot.py
+++ testJackpot.py
@@ -1,17 +1,13 @@
 def testJackpot(x):
-	# y = []
-	# for i in range(len(x)-1):
-	# 	if x[i] == x[i+1]:
-	# 		y.append(True)
-	# 	else:
-	# 		y.append(False)
-	# if False in y:
-	# 	return False
-	# else:
-	# 	return True
-	return True if len(list(set(x))) == 1 else False
-	
-
-	# return [a for a,i in range(len(x)) if x[i] == x[i]+1]
+	y = []
+	for i in range(len(x)-1):
+		if x[i] == x[i+1]:
+			y.append(True)
+		else:
+			y.append(False)
+	if False in y:
+		return False
+	else:
+		return True
 
 print(testJackpot(["&&&&","&&&&","&&&&","&&&&"]))
