6c6f97712255631c35cdc7e217f2e3bff763b436
Naty89 <37753984+Naty89@users.noreply.github.com>
2019-08-10 13:51:42 -0400

    Add files via upload

---
+++ testJackpot_method2.py
@@ -0,0 +1,14 @@
+def testJackpot(x):
+	# y = []
+	# for i in range(len(x)-1):
+	# 	if x[i] == x[i+1]:
+	# 		y.append(True)
+	# 	else:
+	# 		y.append(False)
+	# if False in y:
+	# 	return False
+	# else:
+	# 	return True
+	return True if len(list(set(x))) == 1 else False
+	
+print(testJackpot(["&&&&","&&&&","&&&&","&&&&"]))
